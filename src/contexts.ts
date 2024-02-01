62e52116446b80ee5dabad71b6c9c40173d586a8
David Lougheed <david.lougheed@gmail.com>
2024-02-01 14:32:05 -0500

    refact(context): type context strings to be required

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,19 +1,24 @@
 import { createContext, useContext } from "react";
 
 export interface BentoAuthContextObject {
-    applicationUrl?: string;
-    openIdConfigUrl?: string;
-    clientId?: string;
-    scope?: string;
+    applicationUrl: string;
+    openIdConfigUrl: string;
+    clientId: string;
+    scope: string;
     // Application URLs:
-    postSignOutUrl?: string;
-    authCallbackUrl?: string;
+    postSignOutUrl: string;
+    authCallbackUrl: string;
 }
 
 export const DEFAULT_AUTH_SCOPE = "openid email";
 
 const defaultContextObject: BentoAuthContextObject = {
+    applicationUrl: "",  // default must be false-y for 'missing context' error detection
+    openIdConfigUrl: "",  // default must be false-y for 'missing context' error detection
+    clientId: "",  // default must be false-y for 'missing context' error detection
     scope: DEFAULT_AUTH_SCOPE,
+    postSignOutUrl: "",  // default must be false-y for 'missing context' error detection
+    authCallbackUrl: "",  // default must be false-y for 'missing context' error detection
 };
 
 export const BentoAuthContext = createContext<BentoAuthContextObject>(defaultContextObject);
